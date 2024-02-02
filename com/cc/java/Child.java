933d88e8083435def0bf35bfa650918f1f2e6363
Nekroid <philipleinung@aol.de>
2024-02-02 10:56:53 +0100

    Notiz ++

--- com/cc/java/Child.java
+++ com/cc/java/Child.java
@@ -1,6 +1,7 @@
 package com.cc.java;
 
-public class Child extends Parent{
+public class Child extends Parent // Vererbt an ...
+{
 
 	public void sayHello() {
 		System.out.println("Hello from Child");
